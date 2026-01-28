2ea6df2740cdf891c16f9fd88b97fa447d3cb149
Om <ommuddebihal10@gmail.com>
2026-01-28 15:39:08 +0530

    Initial MLOps project setup

--- src/train.py
+++ src/train.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import joblib
+import mlflow
+import mlflow.sklearn
 import os
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 
+# Load data
 df = pd.read_csv("data/raw/diabetes.csv")
 
 X = df.drop("Diabetes_012", axis=1)
@@ -13,15 +18,25 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, stratify=y, random_state=42
 )
 
-model = RandomForestClassifier(
-    n_estimators=200,
-    class_weight="balanced",
-    random_state=42
-)
+with mlflow.start_run():
 
-model.fit(X_train, y_train)
+    model = RandomForestClassifier(
+        n_estimators=200,
+        class_weight="balanced",
+        random_state=42
+    )
 
-os.makedirs("models", exist_ok=True)
-joblib.dump(model, "models/model.pkl")
+    model.fit(X_train, y_train)
 
-print("Model trained and saved.")
+    preds = model.predict(X_test)
+    f1 = f1_score(y_test, preds, average="macro")
+
+    mlflow.log_param("n_estimators", 200)
+    mlflow.log_metric("f1_macro", f1)
+
+    os.makedirs("models", exist_ok=True)
+    joblib.dump(model, "models/model.pkl")
+
+    mlflow.sklearn.log_model(model, "model")
+
+    print("Model trained. F1:", f1)
